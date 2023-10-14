6bb43b5820c4a3b655748e092a2f52f9d492b01d
wangyuyao438 <wwangang@outlook.com>
2023-10-14 12:08:27 +0800

    Site updated: 2023-10-14 12:08:27

--- build.sh
+++ build.sh
@@ -7,6 +7,8 @@ npm install
 rm -rf source/_posts
 mkdir -p source/_posts
 git clone https://github.com/solstice23/hexo-theme-argon/ theme/argon
+git clone https://github.com/jerryc127/hexo-theme-butterfly/ theme/butterfly
+npm i hexo-renderer-pug hexo-renderer-stylus
 npm un hexo-renderer-marked --save
 npm i hexo-renderer-markdown-it --save
 npm i hexo-image-link --save
