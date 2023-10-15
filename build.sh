00f0ad29ed47148a0ba20bdbdd0f26fc89b5e546
wangyuyao438 <wwangang@outlook.com>
2023-10-15 09:07:23 +0800

    Site updated: 2023-10-15 09:07:23

--- build.sh
+++ build.sh
@@ -1,12 +1,11 @@
 npm install -g hexo-cli           
 mkdir hexo                 
-cd hexo
+cd hexo                            
 hexo cl             
 hexo init             
 npm install                  
 rm -rf source/_posts               
-mkdir -p source/_posts                  
-git clone https://github.com/solstice23/hexo-th eme-argon/ theme/argon                
+mkdir -p source/_posts                                 
 git clone https://github.com/jerryc127/hexo-theme-butterfly/ theme/butterfly                   
 npm i hexo-renderer-pug hexo-renderer-stylus                   
 npm un hexo-renderer-marked --save                      
