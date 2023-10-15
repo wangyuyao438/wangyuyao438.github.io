5ae22d7fef4330df75a8aae31172396fae897732
wangyuyao438 <wwangang@outlook.com>
2023-10-15 08:15:07 +0800

    Site updated: 2023-10-15 08:15:07

--- build.sh
+++ build.sh
@@ -1,18 +1,18 @@
-npm install -g hexo-cli
-mkdir hexo
+npm install -g hexo-cli           
+mkdir hexo                 
 cd hexo
-hexo cl
-hexo init
-npm install
-rm -rf source/_posts
-mkdir -p source/_posts
-git clone https://github.com/solstice23/hexo-theme-argon/ theme/argon
-git clone https://github.com/jerryc127/hexo-theme-butterfly/ theme/butterfly
-npm i hexo-renderer-pug hexo-renderer-stylus
-npm un hexo-renderer-marked --save
-npm i hexo-renderer-markdown-it --save
-npm i hexo-image-link --save
-cd ../
-cp -r `ls | grep -v hexo | grep -v _config.yml | xargs` hexo/source/_posts
-cp _config.yml hexo
-cd hexo && hexo g --config _config.yml
+hexo cl             
+hexo init             
+npm install                  
+rm -rf source/_posts               
+mkdir -p source/_posts                  
+git clone https://github.com/solstice23/hexo-th eme-argon/ theme/argon                
+git clone https://github.com/jerryc127/hexo-theme-butterfly/ theme/butterfly                   
+npm i hexo-renderer-pug hexo-renderer-stylus                   
+npm un hexo-renderer-marked --save                      
+npm i hexo-renderer-markdown-it --save          
+npm i hexo-image-link --save           
+cd ../             
+cp -r `ls | grep -v hexo | grep -v _config.yml | xargs` hexo/source/_posts          
+cp _config.yml hexo              
+cd hexo && hexo g --config _config.yml                   
