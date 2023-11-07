31e5673862bfc0c8e0f022e37ad357a2cb357eb5
wangyuyao438 <wwangang@outlook.com>
2023-11-07 13:19:38 +0800

    Site updated: 2023-11-07 13:19:38

--- build.sh
+++ build.sh
@@ -1,1 +1,17 @@
-cd hexo && hexo cl && hexo g
+npm install -g hexo-cli
+mkdir hexo
+cd hexo
+hexo cl
+npm install
+rm -rf source/_posts
+mkdir -p source/_posts
+git clone https://github.com/jerryc127/hexo-theme-butterfly/ theme/butterfly
+npm i hexo-renderer-pug hexo-renderer-stylus --save
+npm un hexo-renderer-marked --save
+npm i hexo-renderer-markdown-it --save
+npm i hexo-image-link --save
+npm i hexo-electric-clock --save
+cd ../ 
+cp -r `ls | grep -v hexo | grep -v config.yml | xargs` hexo/source/_posts
+cp config.yml hexo
+cd hexo && hexo g --config config.yml
